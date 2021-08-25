d89bc274ea64d17751cd3bf98e586a3c533a6e43
Travis Hance <tjhance7@gmail.com>
2021-08-25 10:57:02 -0400

    work on rwlock

--- src/tpcms/rwlock.v
+++ src/tpcms/rwlock.v
@@ -147,6 +147,26 @@ Definition I {M} `{!EqDecision M} `{!TPCM M} (rw: RwLock M) :=
 Definition rw_mov {M} `{!EqDecision M} `{!TPCM M} (a b : RwLock M) :=
   ∀ p, I_defined (a ⋅ p) -> I_defined (b ⋅ p) /\ I (a ⋅ p) = I (b ⋅ p).
 
+Lemma rw_unit_dot (M: Type) `{!EqDecision M} (a : RwLock M) :
+  a ⋅ (rw_unit M) = a.
+Proof.
+  unfold rw_unit. destruct a. unfold "⋅", rw_op. unfold "⋅", exc_op, free_op.
+  f_equal; trivial.
+  - destruct e; trivial.
+  - destruct e0; trivial.
+  - destruct e1; trivial.
+  - lia.
+  - destruct f; trivial.
+Qed.
+
+Lemma rw_init {M} `{!EqDecision M} `{!TPCM M} (x: M)
+  : V (Central false 0 x).
+Proof.
+  unfold V. exists (rw_unit M). rewrite rw_unit_dot.
+  unfold P, Central, free_count. split; trivial.
+  - intuition; discriminate.
+Qed.
+
 Lemma rw_mov_exc_begin {M} `{!EqDecision M} `{!TPCM M} rc x
   : rw_mov (Central false rc x) (Central true rc x ⋅ ExcPending).
 Proof.
@@ -160,4 +180,132 @@ Proof.
     + unfold P in *. unfold Central, ExcPending in *. destruct p.
       unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f; try contradiction; crush.
 Qed.
-  
+
+Definition rw_exchange_cond {M} `{!EqDecision M} `{!TPCM M}
+    (f: RwLock M) (m: M) (f': RwLock M) (m': M) :=
+  ∀ p ,
+    I_defined (f ⋅ p) -> I_defined (f' ⋅ p) /\ mov (dot m (I (f ⋅ p))) (dot m' (I (f' ⋅ p))).
+
+Lemma rw_mov_exc_acquire {M} `{!EqDecision M} `{!TPCM M} (exc: bool) (x: M)
+  : rw_exchange_cond
+    (Central exc 0 x ⋅ ExcPending)
+    (unit: M)
+    (Central exc 0 x ⋅ ExcGuard)
+    x.
+Proof.
+  unfold rw_exchange_cond. intro. intro. split.
+  - unfold I_defined, "⋅", rw_op, Central, ExcGuard, ExcPending in *. destruct p.
+      unfold "⋅", exc_op, free_op in *.  right. destruct H.
+      + exfalso. unfold rw_unit in H. destruct e, e0, e1, f; inversion H.
+      + destruct e, e0, e1, f; unfold P; intuition; crush. destruct exc; intuition.
+          * destruct u. intuition.
+          * discriminate.
+  - rewrite unit_dot_left. unfold I, I_defined in *. unfold "⋅", Central, ExcPending, ExcGuard, rw_op in *.
+      destruct p. unfold "⋅", free_op, exc_op in *. destruct e, e1, e0; trivial;
+        try (rewrite unit_dot);
+        try (rewrite unit_dot_left);
+        try (apply reflex);
+        unfold P in H; destruct f; try (destruct u); try (destruct exc); unfold rw_unit in *; intuition; try (inversion H0).
+Qed.
+
+Lemma rw_exc_release {M} `{!EqDecision M} `{!TPCM M} (exc: bool) (rc: Z) (x y: M)
+  : rw_exchange_cond
+    (Central exc rc x ⋅ ExcGuard)
+    x
+    (Central false rc x)
+    (unit: M).
+Proof.
+  unfold rw_exchange_cond. intro. intro. split.
+  - unfold I_defined, "⋅", rw_op, Central, ExcGuard, ExcPending in *. destruct p.
+      unfold "⋅", exc_op, free_op in *.  right. destruct H.
+      + exfalso. unfold rw_unit in H. destruct e, e0, e1, f; inversion H.
+      + destruct e, e0, e1, f; unfold P; intuition; crush. destruct exc; intuition.
+          * destruct u. intuition.
+  - rewrite unit_dot_left. unfold I, I_defined in *. unfold "⋅", Central, ExcPending, ExcGuard, rw_op in *.
+      destruct p. unfold "⋅", free_op, exc_op in *. destruct e, e1, e0; trivial;
+        try (rewrite unit_dot);
+        try (rewrite unit_dot_left);
+        try (apply reflex);
+        unfold P in H; destruct f; try (destruct u); try (destruct exc); unfold rw_unit in *; intuition; try (inversion H0).
+Qed.
+
+Lemma rw_lock_shared_begin {M} `{!EqDecision M} `{!TPCM M} (exc: bool) (rc: Z) (x: M)
+  : rw_mov
+    (Central exc rc x)
+    (Central exc (rc + 1) x ⋅ ShPending).
+Proof.
+  unfold rw_mov. intros. unfold I_defined, I in *.
+  destruct H.
+  - exfalso. unfold "⋅", rw_op, rw_unit, Central in H. destruct p. inversion H.
+      unfold "⋅", exc_op in H1. destruct e; discriminate.
+  - split.
+    + right. unfold P in *. unfold Central, ShPending in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f; try contradiction; crush.
+    + unfold P in *. unfold Central, ShPending in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f; try contradiction; crush.
+Qed.
+
+Lemma rw_lock_shared_acquire {M} `{!EqDecision M} `{!TPCM M} (exc: bool) (rc: Z) (x: M)
+  : rw_mov
+    (Central false rc x ⋅ ShPending)
+    (Central false rc x ⋅ ShGuard x).
+Proof.
+  unfold rw_mov. intros. unfold I_defined, I in *.
+  destruct H.
+  - exfalso. unfold "⋅", rw_op, rw_unit, Central in H. destruct p. inversion H.
+  - split.
+    + right. unfold P in *. unfold Central, ShPending, ShGuard in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f; try contradiction; crush.
+      case_decide; try contradiction. crush.
+    + unfold P in *. unfold Central, ShPending, ShGuard in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f; try contradiction; crush.
+Qed.
+
+Lemma rw_lock_shared_release {M} `{!EqDecision M} `{!TPCM M} (exc: bool) (rc: Z) (x y: M)
+  : rw_mov
+    (Central exc rc x ⋅ ShGuard y)
+    (Central exc (rc - 1) x).
+Proof.
+  unfold rw_mov. intros. unfold I_defined, I in *.
+  destruct H.
+  - exfalso. unfold "⋅", rw_op, rw_unit, Central in H. destruct p. inversion H.
+      unfold "⋅", exc_op in H1. destruct e; discriminate.
+  - split.
+    + right. unfold P in *. unfold Central, ShGuard in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f, exc; try contradiction; try case_decide; crush.
+    + unfold P in *. unfold Central, ShGuard in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f; try contradiction; crush.
+Qed.
+
+Lemma rw_lock_shared_retry {M} `{!EqDecision M} `{!TPCM M} (exc: bool) (rc: Z) (x: M)
+  : rw_mov
+    (Central exc rc x ⋅ ShPending)
+    (Central exc (rc - 1) x).
+Proof.
+  unfold rw_mov. intros. unfold I_defined, I in *.
+  destruct H.
+  - exfalso. unfold "⋅", rw_op, rw_unit, Central in H. destruct p. inversion H.
+  - split.
+    + right. unfold P in *. unfold Central, ShPending in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f, exc; try contradiction; try case_decide; crush.
+    + unfold P in *. unfold Central, ShPending in *. destruct p.
+      unfold "⋅", rw_op in *. unfold "⋅", exc_op, free_op in *. destruct e, e0, e1, f; try contradiction; crush.
+Qed.
+
+Definition rw_borrow_back_cond {M} `{!EqDecision M} `{!TPCM M} (f: RwLock M) (m: M)
+  := ∀ p ,
+    I_defined (f ⋅ p) -> ∃ z , (dot m z) = I (f ⋅ p).
+
+Lemma rw_lock_shared_borrow {M} `{!EqDecision M} `{!TPCM M} (x: M)
+  : rw_borrow_back_cond (ShGuard x) x.
+Proof.
+  unfold rw_borrow_back_cond. intros. exists unit. rewrite unit_dot.
+  unfold ShGuard, "⋅", rw_op, I, I_defined in *. destruct p. destruct H.
+  - exfalso. unfold rw_unit in H. inversion H. unfold "⋅" in H5. unfold free_op in H5.
+      destruct f; try discriminate. case_decide; try discriminate.
+  - unfold "⋅", exc_op, free_op in *. unfold P in H. destruct e, e0, e1, f; try contradiction;
+      try (case_decide); try contradiction; try (destruct p); try (destruct p);
+      try intuition; try (destruct u); try contradiction; crush.
+      + destruct u0. crush.
+      + destruct u0. crush.
+Qed.
