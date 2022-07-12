168b246847fe12cf5100e0d2f5c0377e0fd4b9fb
Travis Hance <tjhance7@gmail.com>
2022-07-12 11:45:07 -0400

    finish authfrag utils

--- src/two/protocol.v
+++ src/two/protocol.v
@@ -1,36 +1,20 @@
 From iris.algebra Require Export cmra updates.
 From iris.algebra Require Import proofmode_classes.
 From iris.algebra Require Import auth.
-From iris.algebra Require Import functions.
-From iris.algebra Require Import gmap.
 From iris.prelude Require Import options.
 
-From iris.base_logic Require Import upred.
 From iris.base_logic.lib Require Export own iprop.
 From iris.proofmode Require Import base.
 From iris.proofmode Require Import ltac_tactics.
 From iris.proofmode Require Import tactics.
 From iris.proofmode Require Import coq_tactics.
 
-From iris.base_logic.lib Require Export invariants.
-
-From iris.base_logic.lib Require Export fancy_updates.
-From iris.base_logic.lib Require Export fancy_updates_from_vs.
-
-From iris.proofmode Require Import coq_tactics reduction.
-From iris.proofmode Require Export tactics.
-From iris.program_logic Require Import atomic.
-From iris.prelude Require Import options.
-
-From iris.base_logic.lib Require Export wsat.
-
-From iris.bi Require Import derived_laws.
+(*From iris.base_logic.lib Require Export wsat.*)
 
 Require Import Two.inved.
 Require Import Two.guard.
 Require Import Two.auth_frag_util.
 
-From iris.base_logic Require Import bi.
 
 (*
 Context {Σ: gFunctors}.
